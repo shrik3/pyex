56f0898779af3e088aaf00f51238747ded4eb7c4
shrik3 <cshrik3@gmail.com>
2015-12-21 18:10:38 +0800

    added app.py

---
+++ app.py
@@ -0,0 +1,43 @@
+import logging; logging.basicConfig(level=logging.INFO )
+
+import aiomysql
+import  asyncio,os,json,time
+from datetime import datetime
+from aiohttp import web
+
+
+def index(request):
+    return web.Response(body=b'<h1>Awesome</h1>')
+
+
+@asyncio.coroutine
+def init(loop):
+    app = web.Application(loop=loop)
+    app.router.add_route('GET', '/', index)
+    srv = yield from loop.create_server(app.make_handler(), '127.0.0.1', 9000)
+    logging.info('server started at http://127.0.0.1:9000...')
+    return srv
+
+loop = asyncio.get_event_loop()
+loop.run_until_complete(init(loop))
+loop.run_forever()
+
+
+@asyncio.coroutine
+def create_pool(loop, **kw):
+    logging.info('create database connection pool...')
+    global __pool
+    __pool = yield from aiomysql.create_pool(
+        host=kw.get('host', 'localhost'),
+        port=kw.get('port', 3306),
+        user=kw.get('user'),
+        password=kw.get('password'),
+        db=kw.get('db'),
+        charset=kw.get('charset', 'utf18'),
+        autocommit=kw.get('autocommit', True),
+        maxsize=kw.get('autocommit', 10),
+        minsize=kw.get('minsize',1),
+        loop=loop
+    )
+
+
